873e1ece151cfd225cfc2631bb51cf99105c09ec
Robin Gareus <robin@gareus.org>
2010-10-05 12:06:16 +0200

    new map: South campus

--- pages/contact.php
+++ pages/contact.php
@@ -11,9 +11,9 @@ lac -at- linuxaudio -dot- org
 <h1>Map</h1>
 
 <object id="map" type="text/html" style="width:480px;height:400px;"
-data="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=National+University+of+Ireland,+Maynooth,+Ireland&amp;sll=37.0625,-95.677068&amp;sspn=36.231745,56.513672&amp;ie=UTF8&amp;hq=&amp;hnear=National+University+of+Ireland,+Maynooth,+County+Kildare,+Ireland&amp;ll=53.381639,-6.595144&amp;spn=0.008959,0.018239&amp;z=15&amp;output=embed">
+data="http://maps.google.com/maps/ms?hl=en&amp;ie=UTF8&amp;msa=0&amp;msid=111182610729576284701.000491db5c764b7f1498b&amp;ll=53.387117,-6.585617&amp;spn=0.035833,0.072956&amp;z=13&amp;iwloc=000491db665a3a6618262&amp;output=embed">
 </object>
-<br /><small><a href="http://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=National+University+of+Ireland,+Maynooth,+Ireland&amp;sll=37.0625,-95.677068&amp;sspn=36.231745,56.513672&amp;ie=UTF8&amp;hq=&amp;hnear=National+University+of+Ireland,+Maynooth,+County+Kildare,+Ireland&amp;ll=53.381639,-6.595144&amp;spn=0.008959,0.018239&amp;z=14" style="color:#0000FF;text-align:left;" rel="external">View Larger Map</a></small>
+<br /><small><a href="http://maps.google.com/maps/ms?hl=en&amp;ie=UTF8&amp;msa=0&amp;msid=111182610729576284701.000491db5c764b7f1498b&amp;ll=53.387117,-6.585617&amp;spn=0.035833,0.072956&amp;z=13&amp;iwloc=000491db665a3a6618262&amp;source=embed" style="color:#0000FF;text-align:left;" rel="external">View Larger Map</a></small>
 
 <!--[if lte IE 8]><script type="text/javascript">(function(){var o=document.
 getElementById('map');var i=document.createElement('iframe');i.setAttribute
