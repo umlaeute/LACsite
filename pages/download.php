83d8a68461af4f85c37451a1c3a0d34cbdebf015
Robin Gareus <robin@gareus.org>
2010-10-05 23:57:09 +0200

    added download page

---
+++ pages/download.php
@@ -0,0 +1,31 @@
+<h1>Conference Material Download</h1>
+<p>Moving along with the conference schedule, we're collecting slides and workshop-material to be made available for remote participants and later archiving.</p>
+<p>Most slides are directly linked from the program-entry (click on the <a href="?page=program">table-entry</a> or use the <a href="?page=program&mode=list">list-view</a>). However due to diversity of the content, last-minute hand-ins and logistics, some files are dumped at <a href="http://lac.linuxaudio.org/2011/download/">http://lac.linuxaudio.org/2011/download/</a> indexed below:</p>
+<?php
+  dirlisttable('download');
+?>
+<? /* POST CONFERENCE
+<h2>LAC 2011</h2>
+<p><em>Note</em> that all A/V is provided in Ogg/Theora/Vorbis. Should you have problems playing these, consult Wikipedia's <a href="http://en.wikipedia.org/wiki/Wikipedia:Media_help_%28Ogg%29">OGG media help</a>.<br/> <br/>
+</p>
+<ul>
+ <li>Slides, Papers and Recordings of every talk are linked from the <a href="?page=program&mode=list">Program</a>.</li>
+ <li><a href="http://lac.linuxaudio.org/2011/download/lac2011_proceedings.pdf">Proceedings 2011</a> (17MB PDF)</li>
+ <li><a href="flyer_front.pdf">Flyer: front (PDF)</a></li>
+ <li><a href="flyer_back.pdf">Flyer: back (PDF)</a></li>
+</ul>
+
+</ul>
+</p>
+
+<h2>Links to prior Linux Audio Conferences</h2>
+<ul>
+  <li><a href="http://lac.linuxaudio.org/2010/?page=download">Proceedings &amp; Recordings 2010</a></li>
+  <li><a href="http://lad.linuxaudio.org/events/2009_cdm/">Proceedings &amp; Recordings 2009</a></li>
+  <li><a href="http://lac.linuxaudio.org/2008/download/papers/">Proceedings 2008</a></li>
+  <li><a href="http://lac.linuxaudio.org/2007/download/lac07_proceedings.pdf">Proceedings 2007</a></li>
+  <li><a href="http://lac.zkm.de/2006/papers/lac2006_proceedings.pdf">Proceedings 2006</a></li>
+  <li><a href="http://lac.zkm.de/2005/papers/lac2005_proceedings.pdf">Proceedings 2005</a></li>
+  <li><a href="http://lac.zkm.de//2004/proceedings/lac2004_proceedings.pdf">Proceedings 2004</a></li>
+	</ul>
+ */
