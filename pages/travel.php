eb9e2f677f014986bcc6041509363e3a107a5d64
Robin Gareus <robin@gareus.org>
2010-10-06 14:13:59 +0200

    added note about direct taxi trip.

--- pages/travel.php
+++ pages/travel.php
@@ -6,7 +6,7 @@ NUI Maynooth is accessible by public transport. The university is located 15 mil
 <p><a href="http://www.nuim.ie/location/index.shtml" rel="external">NUIM Travel Information Page</a> contains detailed information including timetables for train &amp; bus. For convenience some of it is summarized below:
 </p>
 <ul>
-  <li>The nearest airport to Maynooth is Dublin Airport. Dublin airport has no rail link, so the best public transport option is to get a bus from the front of the terminal, or taxi to the city centre and continue on the bus or train to Maynooth from there.</li>
+  <li>The nearest airport to Maynooth is Dublin Airport. Dublin airport has no rail link, so the best public transport option is to get a bus from the front of the terminal, or taxi to the city centre and continue on the bus or train to Maynooth from there. A direct taxi ride from the airport to Maynooth is approximately EUR50.</li>
 	<li> NUI Maynooth is served by two bus routes - the 66 and the 67a. Both of these buses depart from Pearse St. in Dublin city centre and run frequent services to Maynooth which cost EUR2.10 each way. There are also 66x and 67x express services to and from Maynooth.</li>
 	<li>Maynooth lies on the Western Suburban railway line which serves three city centre stations (Pearse Station, Tara St. and Connolly Station) as well as nine other suburban stations.</li>
 	<li><a href="http://www.nuim.ie/location/transport0910.pdf">NUIM Transport Guide (14MB PDF)</a></li>
