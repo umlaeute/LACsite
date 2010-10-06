829eb4dc38ae206e20352774f707baa89d88e99e
Robin Gareus <robin@gareus.org>
2010-10-06 23:04:47 +0200

    added info on sponsoring.

--- pages/sponsors.php
+++ pages/sponsors.php
@@ -5,13 +5,53 @@ If you want to contribute to the conference and want to know what you
 can sponsor and what we offer in return, please contact the
 conference organisation: lac -at- linuxaudio -dot- org 
 </p>
+<h2>Why should I sponsor the LAC?</h2>
+<p>
+  The Linux Audio Conference is an annual free-of-charge event that is reaching its
+  eighth year now. This international conference has been a meeting place for many of the world leaders
+	in development of non-proprietary professional Audio Visual software.
+</p>
+<p>
+	The LAC was the incubating event
+  for many of the well-established Free and Open Source packages that have made a significant impact
+  on the area, including the 
+<a rel="external" href="http://jackaudio.org/">Jack Connection Kit</a>, 
+<a rel="external" href="http://ardour.org/">Ardour</a> and
+<a rel="external" href="http://qtractor.sourceforge.net/">Qtractor</a>, 
+  just to mention a few.
+</p>
+<p>
+  Along the years, many important technologies were demonstrated at the conference, such as systems
+	for spatial audio (Wave Field Synthesis, Ambisonics), networked audio and realtime processing.
+	LAC featured presentations by key persons involved with Sound Sythesis 
+  (<a rel="external" href="http://www.csounds.com/">Csound</a>, 
+   <a rel="external" href="http://puredata.info/">Pure Data</a>, 
+	 <a rel="external" href="http://www.audiosynth.com/">SuperCollider</a>, etc.),
+	commercial products
+	(<a rel="external" href="http://mixbus.harrisonconsoles.com/">Harrison Consoles</a>,
+	 <a rel="external" href="http://indamixx.com/">Indamixx</a>, 
+   <a rel="external" href="http://64studio.com/">64studio</a>, 
+   <a rel="external" href="http://www.lionstracs.com/">Lionstracs</a>, 
+   <a rel="external" href="http://www.trinityaudiogroup.com/">Trinity</a>, ...), 
+  developer discussions and - of course - concerts.
+  Recently, the conference has also been a host to the Open Video community
+  (<a rel="external" href="http://lumiera.org/">lumiera</a>, 
+   <a rel="external" href="http://openmovieeditor.org/">openmovieeditor</a>,
+   <a rel="external" href="http://www.kdenlive.org/">kdenlive</a>,
+   <a rel="external" href="http://www.kinodv.org/">kino</a>),
+	expanding its range to incorporate visual technologies.
+</p>
+<p>
+  In summary: the LAC is the place for companies looking to interface with the non-proprietary pro-AV community,
+  researchers, developers, artists and users.
+</p>
 <?php /* OLD POST-CONFERENCE
 <h1>Sponsors</h1>
 <p>
 As admittance to the conference was free, several things required sponsoring which was made possible by the following partners:
 </p>
 */?>
-
+<h2>Supporters</h2>
 <table border="0" width="100%" id="supporter">
 <tr>
 <?php
