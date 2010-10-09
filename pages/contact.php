174a9a0c0e81f25f55e5183715a61e6270fadd61
Robin Gareus <robin@gareus.org>
2010-10-09 21:07:24 +0200

    more style fixes.

--- pages/contact.php
+++ pages/contact.php
@@ -10,7 +10,7 @@ lac -at- linuxaudio -dot- org
 
 <h1>Map</h1>
 
-<object id="map" type="text/html" style="width:480px;height:400px;"
+<object id="map" type="text/html" style="width:460px;height:400px;"
 data="http://maps.google.com/maps/ms?hl=en&amp;ie=UTF8&amp;msa=0&amp;msid=111182610729576284701.000491db5c764b7f1498b&amp;ll=53.387117,-6.585617&amp;spn=0.035833,0.072956&amp;z=13&amp;iwloc=000491db665a3a6618262&amp;output=embed">
 </object>
 <br /><small><a href="http://maps.google.com/maps/ms?hl=en&amp;ie=UTF8&amp;msa=0&amp;msid=111182610729576284701.000491db5c764b7f1498b&amp;ll=53.387117,-6.585617&amp;spn=0.035833,0.072956&amp;z=13&amp;iwloc=000491db665a3a6618262&amp;source=embed" style="color:#0000FF;text-align:left;" rel="external">View Larger Map</a></small>
