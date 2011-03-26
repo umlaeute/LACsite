5fd58b5bd4a3fdda060af51624d12bcbce0ea3ba
LAC Organizers <lac@linuxaudio.org>
2011-03-26 18:17:56 +0000

    updates to B&B and Hotel offers

--- pages/travel.php
+++ pages/travel.php
@@ -13,6 +13,30 @@ NUI Maynooth is accessible by public transport. The university is located 15 mil
 </ul>
 
 <h1>Staying in Maynooth</h1>
-<p> Dedicated information about accommodation for LAC2011 will be made available shortly.
-Meanwhile check out the excellent <a href="http://www.nuim.ie/about/accommodation.shtml" rel="external">Visitor Accommodation</a> information page of NUIM.
+<p> Accommodation on Campus is limited, but still available for most nights around the conference dates, except for Saturday night. Please
+check the<a href="http://www.maynoothcampus.com" rel="external"> Maynooth campus website </a> </p>
+<p> Other possibilities in Maynooth can be found in the <a href="http://www.nuim.ie/about/accommodation.shtml" rel="external">Visitor Accommodation</a> information page of NUIM. </p>
+<p> For participants wanting to stay in Dublin, where a larger range of options exist, Maynooth is well connected to the city centre  by a train service (station 5 mins from University) and busses (66 and 67). </p>
+
+<h1>Special Offers</h1>
+<p></b> Kilcar House B&B </b></p>
+<p> <a href="http://www.kilcarhouse.ie" rel="external"> Website</a>
+<p>
+B&B 10 min walk from Campus. Eur31.00 per person per night for Twin and Double Rooms including breakfast. 
+Single rooms are Eur35.00. If any guests do not require breakfast the rates will be less.  All rooms are en-suite  with tv,wireless internet and  tea/coffee facilities.
+</p>
+
+<p> <b>Springfield Hotel</b> </p>
+<p><a href="http://www.springfieldhotel.ie" rel="external"> Website</a> </p>
+<p>
+For LAC participants, rates of Eur55.00 (single) and Eur85.00 (twin), with breakfast.
+Located in Leixlip, which is the next village to Maynooth (10 mins on bus/train).
+</p>
+<p><b>Glenroyal Hotel </b></p>
+<p><a href="http://www.glenroyalhotelkildare.ie" rel="external"> Website </a></p>
+<p>
+For LAC participants, 2 nights B&B + 1 dinner Eur89.00 per person sharing or
+139.00 (single person). Check with  hotel for other offers. The hotel
+is 5 mins walk from University. 
+</p>
 </p>
