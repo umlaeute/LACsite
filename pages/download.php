0d8b7ecd49a6d86e3c381f1cc8439fe8c509e1d8
Robin Gareus <robin@gareus.org>
2011-03-28 22:28:21 +0200

    more XHTML fixes

--- pages/download.php
+++ pages/download.php
@@ -1,6 +1,6 @@
 <h1>Conference Material Download</h1>
 <p>Moving along with the conference schedule, we're collecting slides and workshop-material to be made available for remote participants and later archiving.</p>
-<p>Most slides are directly linked from the program-entry (click on the <a href="?page=program">table-entry</a> or use the <a href="?page=program&mode=list">list-view</a>). However due to diversity of the content, last-minute hand-ins and logistics, some files are dumped at <a href="http://lac.linuxaudio.org/2011/download/">http://lac.linuxaudio.org/2011/download/</a> indexed below:</p>
+<p>Most slides are directly linked from the program-entry (click on the <a href="?page=program">table-entry</a> or use the <a href="?page=program&amp;mode=list">list-view</a>). However due to diversity of the content, last-minute hand-ins and logistics, some files are dumped at <a href="http://lac.linuxaudio.org/2011/download/">http://lac.linuxaudio.org/2011/download/</a> indexed below:</p>
 <?php
   dirlisttable('download');
 ?>
