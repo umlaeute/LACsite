49fa8e51153525c09fee735f539ff3614c2c81a9
Robin Gareus <robin@gareus.org>
2011-10-05 22:52:37 +0200

    added sitemap.

---
+++ sitemap.php
@@ -0,0 +1,56 @@
+<?php
+define('NL', "\n");
+define('CANONICALURL', "http://lac.linuxaudio.org/2012/");
+header ("Content-Type:text/xml");
+
+echo '<?xml version="1.0" encoding="UTF-8"?>'.NL;
+echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'.NL;
+
+require_once('config.php');
+require_once('site.php');
+
+$sitemap = array();
+
+foreach ($pages as $p => $k) {
+	$sitemap[$p]['priority'] = .5;
+	$sitemap[$p]['changefreq'] = 'daily';
+}
+
+$sitemap['']['priority'] = .8;
+$sitemap['']['changefreq'] = 'weekly';
+$sitemap['about']['priority'] = .8;
+$sitemap['about']['changefreq'] = 'weekly';
+
+$sitemap['participation']['priority'] = .7;
+$sitemap['participation']['changefreq'] = 'weekly';
+
+$sitemap['program']['priority'] = .6;
+$sitemap['program']['changefreq'] = 'weekly';
+$sitemap['contact']['priority'] = .6;
+$sitemap['contact']['changefreq'] = 'monthly';
+
+$sitemap['registration']['priority'] = .3;
+$sitemap['registration']['changefreq'] = 'monthly';
+$sitemap['participants']['priority'] = .3;
+$sitemap['participants']['changefreq'] = 'always';
+
+foreach ($sitemap as $p => $k) {
+
+	if ($userewrite) {
+		echo '  <url>'.NL.'    <loc>'.CANONICALURL.$p.'</loc>'.NL;
+		if (!empty($k['changefreq'])) 
+			echo '    <changefreq>'.$k['changefreq'].'</changefreq>'.NL;
+		if (!empty($k['priority'])) 
+			echo '    <priority>'.$k['priority'].'</priority>'.NL;
+		echo '  </url>'.NL;
+	} 
+	else if (!empty($p)) {
+		echo '  <url>'.NL.'    <loc>'.CANONICALURL.'?page='.$p.'</loc>'.NL;
+		if (!empty($k['changefreq'])) 
+			echo '    <changefreq>'.$k['changefreq'].'</changefreq>'.NL;
+		if (!empty($k['priority'])) 
+			echo '    <priority>'.$k['priority'].'</priority>'.NL;
+		echo '  </url>'.NL;
+	}
+}
+echo '</urlset>'.NL;
