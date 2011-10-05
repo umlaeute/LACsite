6a1291590324b764781e2425c6aac1a2ab9e9744
Robin Gareus <robin@gareus.org>
2011-10-05 19:54:13 +0200

    compress css and js with yui-compressor.

---
+++ static/script_src.js
@@ -0,0 +1,63 @@
+function externalLinks() {
+  if (!document.getElementsByTagName) return;
+  var anchors = document.getElementsByTagName("a");
+  for (var i=0; i<anchors.length; i++) {
+    var anchor = anchors[i];
+    if (anchor.getAttribute("href") &&
+        anchor.getAttribute("rel") == "_blank")
+      anchor.target = "_blank";
+    if (anchor.getAttribute("href") &&
+        anchor.getAttribute("rel") == "external")
+      anchor.target = "_blank";
+    if (anchor.getAttribute("href") &&
+        anchor.getAttribute("rel") == "wiki")
+      anchor.target = "lac2011-wiki";
+    if (anchor.getAttribute("href") &&
+        anchor.getAttribute("rel") == "supporter")
+      anchor.target = "lac2011-sponsor";
+    if (anchor.getAttribute("href") &&
+        anchor.getAttribute("rel") == "registration")
+      anchor.target = "lac2011-registration";
+  }
+}
+window.onload = externalLinks;
+
+function inlineInfoBox(id){
+  document.getElementById('infobox').style.display = "inline";
+  document.getElementById('dimmer').style.display = "inline";
+  document.getElementById('infobox').scrollTop=0; /* FIXME: scrollTop is not compatible */
+}
+
+function showInfoBox(id){
+  if (document.getElementById('ieframe')) {
+    document.getElementById('ieframe').src  = 'raw.php?pdb_filterid='+id;
+  } else {
+    document.getElementById('infoframe').data = 'raw.php?pdb_filterid='+id;
+  }
+  inlineInfoBox();
+}
+
+function showInfoPix(id){
+  if (document.getElementById('ieframe')) {
+    document.getElementById('ieframe').src  = 'pix.php?id='+id;
+  } else {
+    document.getElementById('infoframe').data = 'pix.php?id='+id;
+  }
+  inlineInfoBox();
+}
+function hideInfoBox() {
+  if (document.getElementById('ieframe')) {
+    document.getElementById('ieframe').src  = 'raw.php';
+  } else {
+    document.getElementById('infoframe').data = 'raw.php';
+  }
+  document.getElementById('infobox').style.display = "none";
+  document.getElementById('dimmer').style.display = "none";
+}
+
+function admingo(page,mode,param) {
+	document.getElementById('page').value=page;
+	document.getElementById('mode').value=mode;
+	document.getElementById('param').value=param;
+	document.myform.submit();
+}
