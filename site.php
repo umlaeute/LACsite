c687bc3162190f591b9c8a2538b4ae60e78867b0
Robin Gareus <robin@gareus.org>
2011-10-22 11:01:59 +0200

    added LWN to sponsors

--- site.php
+++ site.php
@@ -37,6 +37,8 @@
 #define sponsors/supportes
   $sponsors = array(
     'http://ccrma.stanford.edu/' => array('img' => 'img/logos/ccrma.png', 'title' => 'CCRMA'),
+    'http://lwn.net/' => array('img' => 'img/logos/lwn.jpg', 'title' => 'LWN.NET'),
+    'http://ccrma.stanford.edu/' => array('img' => 'img/logos/ccrma.png', 'title' => 'CCRMA'),
     'http://arts.stanford.edu/sai.php?section=sica&amp;page=about' => array('img' => 'img/logos/sica.jpg', 'title' => 'SiCA, Stanford'),
     'http://www.stanford.edu/' => array('img' => 'img/logos/stanford.jpg', 'title' => 'Stanford University'),
     'http://linuxaudio.org/' => array('img' => 'img/logos/lao.png', 'title' => 'linuxaudio.org'),
