b261fd659e627a91698617ca2ac61f37980e1bc8
LAC Organizers <lac@linuxaudio.org>
2012-02-14 16:38:44 +0000

    updated travel&stay page.

--- pages/travel.php
+++ pages/travel.php
@@ -3,20 +3,53 @@
 </p>
 
 <h1>Staying in the Bay Area</h1>
-<p>
-Here is <a href="http://www.stanford.edu/dept/visitorinfo/plan/stay.html" rel="external">Stanford Logding guide</a>. 
-</p>
-<p>
-<a href="http://www.hotelscombined.com/Conference_Support">Hotels combined</a> are supporting LAC2012 and offer a 10% discount on all hotels booked through their website.
-</p>
+
+<p>Some help is available in the <a href="http://www.stanford.edu/dept/visitorinfo/plan/stay.html" rel="external">Stanford Logding guide</a>. </p>
+
+<h1>Staying in Palo Alto</h1>
+
+<p>Unless you plan to have a car (and pay for parking at Stanford), you should definitely make sure you find a hotel that is close to the <a href="http://transportation.stanford.edu/marguerite">Marguerite shuttle</a>, the free bus service Stanford provides. If you look at the <a href="http://transportation.stanford.edu/pdf/marguerite-schedule-map.pdf">schedule (PDF)</a> you'll see not all lines run on weekends (check O and N for nightly services and SE for weekend services). You will most likely be taking the Marguerite everyday to get to campus (the Stanford Campus is BIG). Most Marguerite lines stop at the <a href="http://g.co/maps/nsgja">Palo Alto Transit Center</a>, so look for hotels that are walking distance to it. There is another Marguerite line that stops at the <a href="http://g.co/maps/3mxjq">California Avenue Caltrain station</a>, so hotels near California Avenue are also an option.</p>
+
+<p><a href="http://www.hotelscombined.com/Conference_Support">Hotels Combined</a> is supporting LAC2012 and offer a 10% discount on all hotels booked through their website.</p>
+
+<p>Try also:</p>
+<ul>
+<li><a href="http://www.stanford.edu/dept/visitorinfo/plan/stay.html">http://www.stanford.edu/dept/visitorinfo/plan/stay.html</a>
+<li><a href="http://www.hotels.com/">http://www.hotels.com/</a>
+<li><a href="http://www.airbnb.com/">http://www.airbnb.com/</a>
+</ul>
 
 <h1>Traveling to Stanford</h1>
 
-<p>
-CCRMA is housed in the Knoll, 660 Lomita Dr. Stanford, CA  94305. Please use the following directions in conjunction with the Stanford <a href="http://campus-map.stanford.edu/" rel="external">campus map</a> or <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;geocode=&amp;q=660+lomita,+stanford+ca&amp;sll=37.0625,-95.677068&amp;sspn=45.197878,82.265625&amp;ie=UTF8&amp;om=1&amp;z=16&amp;layer=t" rel="external">Google Map</a>.&nbsp;</p>
+<p>CCRMA is housed in the Knoll, 660 Lomita Dr. Stanford, CA  94305. Please use the following directions in conjunction with the Stanford <a href="http://campus-map.stanford.edu/" rel="external">campus map</a> or <a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;geocode=&amp;q=660+lomita,+stanford+ca&amp;sll=37.0625,-95.677068&amp;sspn=45.197878,82.265625&amp;ie=UTF8&amp;om=1&amp;z=16&amp;layer=t" rel="external">Google Map</a>.&nbsp;</p>
 
-<p><strong>via Caltrain</strong></p>
-<p>Take Caltrain to the Palo Alto station. Take <a href="http://transportation.stanford.edu/marguerite/" rel="external">Marguerite</a> shuttle service (either B Line or Midnight Express, depending on time of day) onto the Stanford campus; shuttles meet each train. Get off the shuttle at Tressider Union. Walk away from the Union through the Tressider parking lot and cross Mayfield Avenue. A pathway to the left of Florence Moore Hall leads up a hill. At the top of the hill is Lomita Drive, with the Knoll on your left.</p>
+<h3>From the airports...</h3>
+
+<p>You should fly into either San Francisco Airport (SFO) or San Jose Airport (SJC). From the airport, the best ways to get to Palo Alto are by train or by door-to-door shuttle. Details on each option:</p>
+
+<p><strong>by Train</strong> (total cost around $10)</p>
+
+<p><strong>From SFO</strong></p>
+
+<p>take the BART (10 minute ride, $4 fee) to the Millbrae Station. Depending on the time of the day, you may have to get off at BART San Bruno and board another BART to Millbrae. From Millbrae station, take the Caltrain southbound (towards San Jose). Caltrain will be a 30 minute ride, and it costs $4.25. You will get off either at Palo Alto Caltrain Station, or California Avenue CalTrain Station, depending on where your hotel is. Caltrain website: http://www.caltrain.com/</p>
+
+<p>If you want to go straight to Stanford, get off at Palo Alto Caltrain and take the free Marguerite Shuttle (line X or Y - see http://transportation/marguerite/), and ask to get off at the Tresidder Union stop. Walk away from the Union through the Tresidder parking lot and cross Mayfield Avenue. A pathway to the left of Florence Moore Hall leads up a hill. At the top of the hill is Lomita Drive, with the Knoll on your left.</p>
+
+<p><strong>From SJC</strong></p>
+
+<p>take the VTA Airport Flyer Route #10 towards the Santa Clara Caltrain station. This is a free bus. Note that there are two different Route #10 busses: one goes to the Caltrain Station (that's the one you want), the other goes to Metro Light Rail (don't take this one). The bus has a clear indication of which route it is, so it's easy. When you get off the bus at Santa Clara Caltrain station, take the Caltrain northbound (towards San Francisco). Caltrain will be a 20-30 minute ride, and it costs $4.25. You will get off either at Palo Alto Caltrain Station, or California Avenue CalTrain Station, depending on where your hotel is. Caltrain website: http://www.caltrain.com/</p>
+
+<p>If you want to go straight to Stanford, get off at Palo Alto Caltrain and take the free Marguerite Shuttle (line X or Y - see http://transportation/marguerite/), and ask to get off at Tresidder Union stop. Walk away from the Union through the Tresidder parking lot and cross Mayfield Avenue. A pathway to the left of Florence Moore Hall leads up a hill. At the top of the hill is Lomita Drive, with the Knoll on your left.</p>
+
+<p><strong>Door-to-door Shuttle</strong> (total cost around $30)</p>
+
+<p>You can either make your reservation in advance, or you can just walk out of the airport and look for the designated space where SuperShuttle vans stop; you can get a van from there without a reservation. For more info, please check http://www.supershuttle.com/</p>
+
+<h3>From San Francisco or San Jose</h3>
+
+<p>Take Caltrain to the Palo Alto station. Take the free Marguerite shuttle service (Lines X or Y, depending on time of day) onto the Stanford campus. Get off the shuttle at Tresidder Union. Walk away from the Union through the Tresidder parking lot and cross Mayfield Avenue. A pathway to the left of Florence Moore Hall leads up a hill. At the top of the hill is Lomita Drive, with the Knoll on your left. Caltrain website: http://www.caltrain.com/</p>
+
+<h3>By Car</h3>
 
 <p><strong>from 280</strong></p>
 <p>Exit at Alpine Road heading east. At the first traffic light, turn right onto Junipero Serra Boulevard. At the next traffic light, turn left onto Campus Drive West. At the stop sign, turn right onto Santa Teresa. Continue on Santa Teresa until the second stop sign, then turn right on Lomita Drive. Stay to the right on Lomita; the Knoll is at 660 Lomita.</p>
@@ -30,5 +63,6 @@ CCRMA is housed in the Knoll, 660 Lomita Dr. Stanford, CA  94305. Please use the
 <p><strong>from Foothill Expressway</strong></p>
 <p>Follow Foothill across Page Mill, where it becomes Junipero Serra Boulevard. Turn right onto the Stanford Campus at Campus Drive East. At the first stop sign, turn left onto Mayfield Avenue. Follow Mayfield as it curves left. At the end of Mayfield, turn left on Lomita Drive. The Knoll is at 660 Lomita.</p>
 
-<p><strong>Parking</strong></p>
+<h3>Parking</h3>
+
 <p>Parking in front of and behind CCRMA is zoned for Stanford 'C' permits, Mon-Fri 6am-4pm (i.e., parking is free around the building all other times). The nearest visitor pay parking lot is at Tressider Union (<a href="http://campus-map.stanford.edu/?ID=02-300" rel="external">campus map</a> and <a href="http://maps.google.com/maps?q=459+Lagunita+Dr,+Stanford,+CA%E2%80%8E&amp;oe=utf-8&amp;rls=org.mozilla:en-US:official&amp;client=firefox-a&amp;um=1&amp;ie=UTF-8&amp;hq=&amp;hnear=459+Lagunita+Dr,+Stanford,+CA+94305&amp;gl=us&amp;ei=Ow76SqbCM4aIsgOTx4XCDg&amp;sa=X&amp;oi=geocode_result&amp;ct=title&amp;resnum=1&amp;ved=0CAsQ8gEwAA" rel="external">Google Map</a>), off Mayfield Avenue. For more information about visitor parking on the Stanford campus, please visit the Stanford <a href="http://transportation.stanford.edu/parking_info/VisitorParking.shtml" rel="external">Parking and Transportation</a> website.</p>
