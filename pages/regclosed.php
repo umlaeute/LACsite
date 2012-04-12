da3c5749773a339f6ed89bd72c3ada5388c7a9db
Robin Gareus <robin@gareus.org>
2012-04-12 06:09:57 +0200

    noreg.

--- pages/regclosed.php
+++ pages/regclosed.php
@@ -1,2 +1,3 @@
 <h1>Registration closed</h1>
 <p>The online registration is closed.</p>
+<p>Should you still make it to CCRMA, there will be an option to sign up every morning at the registration desk.</p>
