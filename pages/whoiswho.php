513d5d97bc93954084652ce8a942ce55cd4a8e37
Robin Gareus <robin@gareus.org>
2012-05-02 20:54:33 +0200

    more typos.

--- pages/whoiswho.php
+++ pages/whoiswho.php
@@ -76,4 +76,4 @@ echo '<li>&nbsp;</li><li>&nbsp;</li><li>52. <a href="https://ccrma.stanford.edu/
 echo "</ul>\n";
 echo '<div class="clearer"></div>'."\n";
 echo '<p>If you know - or are - any of the unidentified persons, please <a href="'.local_url('contact').'">drop us a line</a>.</p>';
-echo '<p>The picture was taken right after Dave Phillips\' <a href="'.local_url('program','pdb_filterid=27').'">keynote</a> address, not everybody that came to the conference is in this picture. Sorry, we don\'t <em><a href="http://www.gimp.org/" rel="external">gimp</a></em> you in, your next chance will be LAC\'13 at IEM, Graz.</p>';
+echo '<p>The picture was taken right after Dave Phillips\' <a href="'.local_url('program','pdb_filterid=27').'">keynote</a> address, not everybody that came to the conference is in this picture. Sorry, we won\'t <em><a href="http://www.gimp.org/" rel="external">gimp</a></em> you in, your next chance will be LAC\'13 at IEM, Graz.</p>';
