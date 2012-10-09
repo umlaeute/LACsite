fe7655137710ae1980752a13b32ee70630af80b0
IOhannes m zmölnig <zmoelnig@iem.at>
2012-10-09 17:25:35 +0200

    fixed my name

--- pages/about.php
+++ pages/about.php
@@ -47,7 +47,11 @@ TODO: ABOUT IEM...
 
 <h1>Who?</h1>
 <p>Check out the <a href="<?=local_url('participants')?>">list of participants</a> to see who else is coming.</p>
-<p>LAC is organized by a community of GNU/Linux free/libre software users and developers that communicate through the <a href="http://lists.linuxaudio.org/">Linux-Audio-Users and Linux-Audio-Developers</a> mailing lists. LAC2013 is under the auspices of <a href="http://iem.kug.ac.at/" rel="external">IEM</a>, organized by <a href="http://puredata.info/author/zmoelnig" rel="external">IOhannes m. Zm&ouml;lnig</a> with assistance of Frank Neumann, <a href="http://gareus.org" rel="external"> Robin Gareus</a> and Florian Hollerweger.</p>
+<p>LAC is organized by a community of GNU/Linux free/libre software users and
+developers that communicate through the <a
+href="http://lists.linuxaudio.org/">Linux-Audio-Users and
+Linux-Audio-Developers</a> mailing lists. LAC2013 is under the auspices of <a
+href="http://iem.kug.ac.at/" rel="external">IEM</a>, organized by <a href="http://umlaeute.mur.at" rel="external">IOhannes m zm&ouml;lnig</a> with assistance of Frank Neumann, <a href="http://gareus.org" rel="external"> Robin Gareus</a> and Florian Hollerweger.</p>
 
 <?php
 /*
@@ -68,7 +72,7 @@ TODO: ABOUT IEM...
 &nbsp;&nbsp; Bruno Ruviaro, <em>CCRMA, United States</em><br/>
 &nbsp;&nbsp; Julius Smith, <em>CCRMA, United States</em><br/>
 &nbsp;&nbsp; Michael Wilson, <em>CCRMA, United States</em><br/>
-&nbsp;&nbsp; IOhannes Zmölnig, <em>Institute of Electronic Music and Acoustics (IEM@KUG), Austria</em><br/>
+&nbsp;&nbsp; IOhannes zmölnig, <em>Institute of Electronic Music and Acoustics (IEM@KUG), Austria</em><br/>
 </p>
  */
 ?>
