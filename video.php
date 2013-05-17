dfdf772230079e331257b058b84d8f00f63eff49
Robin Gareus <robin@gareus.org>
2013-05-17 18:47:15 +0200

    first iteration of video playback webm/mp4..

--- video.php
+++ video.php
@@ -14,17 +14,21 @@ function printerror($msg) {
   exit;
 }
 
-$allowlive=false; # $config['allowlivevideo'];
 $id=0;
+$height=720;
+$width=1280;
 
 if (!isset($_REQUEST['id'])) {
-  if (!$allowlive) 
-    printerror('invalid request - no id specified.');
-  else {
-    $id = -1;
-  }
+  printerror('invalid request - no id specified.');
 } else {
   $id=intval(rawurldecode($_REQUEST['id']));
+}
+if (isset($_REQUEST['h'])) {
+  $h=intval(rawurldecode($_REQUEST['h']));
+  if ($h == 720) { $height=720; $width=1280; }
+  else if ($h == 480) { $height=480; $width=854; }
+  else if ($h == 360) { $height=360; $width=640; }
+  else if ($h == 240) { $height=240; $width=428; }
 }
 
 if ($id > 0) {
@@ -43,110 +47,22 @@ if ($id > 0) {
   if (count($result) ==0)
     printerror('No matching entries found.');
   $v=$result[0];
-} elseif ($id == -1 && $allowlive) {
-  $v=array(
-    'title' => 'Live Stream - High Quality',
-    'url_stream' => 'http://ccrma.stanford.edu:8080/lac2012-hq.ogv',
-    'url_slides' => 'http://lac.linuxaudio.org/2012/files',
-    'url_paper' => '',
-    'url_misc' => '',
-    'url_abstract' => '',
-  );
-} elseif ($id == -2 && $allowlive) {
-  $v=array(
-    'title' => 'Live Stream - Low Quality',
-    'url_stream' => 'http://ccrma.stanford.edu:8080/lac2012-lq.ogv',
-    'url_slides' => 'http://lac.linuxaudio.org/2012/files',
-    'url_paper' => '',
-    'url_misc' => '',
-    'url_abstract' => '',
-  );
-} elseif ($id == -3 && $allowlive) {
-  $v=array(
-    'title' => 'Live Stream - High Quality',
-    'url_stream' => 'http://streamer.stackingdwarves.net:8000/lac2012-hq.ogv',
-    'url_slides' => 'http://lac.linuxaudio.org/2012/files',
-    'url_paper' => '',
-    'url_misc' => '',
-    'url_abstract' => '',
-  );
-} elseif ($id == -4 && $allowlive) {
-  $v=array(
-    'title' => 'Live Stream - Low Quality',
-    'url_stream' => 'http://streamer.stackingdwarves.net:8000/lac2012-lq.ogv',
-    'url_slides' => 'http://lac.linuxaudio.org/2012/files',
-    'url_paper' => '',
-    'url_misc' => '',
-    'url_abstract' => '',
-  );
 } else {
   printerror('No matching entries found.');
 }
 
 if ($config['hidepapers']) $v['url_paper'] = '';
 
-$w=720;$h=576;
+
 $url=$v['url_stream'];
 
-if (in_array($id, array(30,31,37,55,59,68,73,83,84,85))) { # 16:9 recordings
-  $w=854;$h=480;
-}
-if (isset($v['width']))  $w=$v['width'];
-if (isset($v['height'])) $h=$v['height'];
-
-$ua=$_SERVER['HTTP_USER_AGENT'];
-$jar=BASEURL.'static/cortado_url.jar';
-#print_r($url);
-#print_r($ua);
-
-
-if (stripos($ua, 'firefox')!== false 
-/*|| stripos($ua, 'chromium')!== false*/
-/*|| stripos($ua, 'chrome')!== false*/
-/*|| stripos($ua, 'opera')!== false*/
-) {  /* Browser HTML-5 */
-  $out='
-  <video width="'.$w.'" height="'.$h.'" autoplay loop controls>
-    <source src="'.$url.'"  type="video/ogg" />
-  </video>
-';
-}
-else { /* JAVA - Cortado player */
-  $useobject=1;
-
-  $params='
-    <param name="archive" value="'.$jar.'" />
-    <param name="url" value="'.$url.'"/>
-    <param name="keepaspect" value="true"/>
-    <param name="local" value="false"/>
-    <param name="statusHeight" value="18"/>
-    <param name="live" value="false"/>
-    <param name="seekable" value="true"/>
-    <param name="bufferSize" value="200"/>
-    <param name="duration" value="0"/>
-';
-
-  if ($useobject==0) {
-    $out='
-  <applet code="com.fluendo.player.Cortado.class" archive="'.$jar.'" width="'.$w.'" height="'.$h.'">
-'.$params.'
-  </applet>
-';
-  } else {
-    $out='
-  <object type="application/x-java-applet" data='.$jar.' width="'.$w.'" height="'.$h.'" standby="Loading Video Player..">
-    <param name="classid" value="java:com.fluendo.player.Cortado.class" />
-'.$params.'
-    <p>
-      <strong>
-        This browser does not have a Java Plug-in.<br/>
-        <a href="http://www.java.com/getjava" title="Download Java Plug-in">Get the latest Java Plug-in here.</a>
-      </strong>
-    </p>
-  </object>
-';
-  }
-}
+# TODO use strreplace or sth faster (or update database)
+$url=preg_replace('@\.webm$@', '', $url);
+$url=preg_replace('@\.mp4$@', '', $url);
+$url=preg_replace('@-720p$@', '', $url);
+$url=preg_replace('@-480p$@', '', $url);
+$url=preg_replace('@-360p$@', '', $url);
+$url=preg_replace('@-240p$@', '', $url);
 
 echo '<div class="header">Linux Audio Conference '.LACY.'</div>';
 echo '<div class="title">';
@@ -162,30 +78,37 @@ if ($id > 0) {
 }
 echo '</div>';
 if (!empty($url)) {
-  echo '<div class="player">'.$out.'</div>';
-  if ($id > 0) {
-    $tu=rawurlencode($url);
-    $tt=rawurlencode(SHORTTITLE.' - '.$v['title']);
-    echo '
-  <div id="sociallinkbar">
-    <a id="lnktwtr" rel="external" href="http://twitter.com/share?url='.$tu.'" title="Tweet This!"></a>
-    <a id="lnkturl" rel="external" href="http://tinyurl.com/create.php?url='.$tu.'" title="Create TinyURL"></a>
-    <a id="lnkbtly" rel="external" href="http://bit.ly/?url='.$tu.'" title="Create bit.ly shortcut"></a>
-    <a id="lnkfbok" rel="external" href="http://www.facebook.com/sharer.php?u='.$tu.'&amp;t='.$tt.'" title="Send to Facebook"></a>
-    <a id="lnkgogl" rel="external" href="https://www.google.com/bookmarks/mark?op=add&amp;bkmk='.$tu.'&amp;title='.$tt.'&amp;annotation=&amp;nui=1&amp;service=bookmarks" title="Send to Google"></a>
-    <a id="lnksupn" rel="external" href="http://www.stumbleupon.com/submit?url='.$tu.'&amp;title='.$tt.'" title="Submit to Stumble Upon"></a>
-    <a id="lnkdlus" rel="external" href="http://delicious.com/save?v=6&amp;jump=yes&amp;url='.$tu.'&amp;title='.$tt.'" title="Bookmark this on Del.icio.us"></a>
-  </div>
-';
+  echo '</div>'; # container
+  echo '<div class="player" style="width:'.$width.'px;">';
+  echo '<video width="'.$width.'" height="'.$height.'" autoplay loop controls>
+    <source src="'.$url.'-'.$height.'p.webm"  type="video/webm" />
+    <source src="'.$url.'-'.$height.'p.mp4"  type="video/mp4" />
+  </video>';
+  echo '</div>';
+  echo '<div class="container">';
+  echo '<div id="sizebar">';
+  foreach (array(240, 360, 480, 720) as $s) {
+    if ($s == $height)
+      echo '&nbsp;'.$s.'p&nbsp;';
+    else
+      echo '&nbsp;<a href="video.php?id='.$id.'&amp;h='.$s.'">'.$s.'p</a>&nbsp;';
+
   }
+  echo '</div>';
 } else {
   echo '<div class="error"><br/>This presentations has not been recorded.</div>';
 }
 
 echo '<div class="links"><ul>';
 
-if (!empty($url))
-  echo '<li>Video URL: <a href="'.$url.'">'.basename($url).'</a></li>';
+if (!empty($url)) {
+  echo '<li>Video URL: ';
+  foreach (array(240, 360, 480, 720) as $s) {
+    echo '<a href="'.$url.'-'.$s.'p.mp4">'.$s.'p mp4</a>&nbsp;';
+    echo '<a href="'.$url.'-'.$s.'p.webm">'.$s.'p webm</a>&nbsp;';
+  }
+  echo '</li>';
+}
 if (!empty($v['url_slides']))
   echo '<li>Slides: <a href="'.$v['url_slides'].'" rel="external">'.$v['url_slides'].'</a></li>';
 if (!empty($v['url_paper']))
