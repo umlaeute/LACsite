b314bbd5e03e94a2a3c4f83cffd902c6586eefb0
LAC Organizers <lac@linuxaudio.org>
2013-06-13 07:35:39 +0000

    count missing names

--- pages/whoiswho.php
+++ pages/whoiswho.php
@@ -116,11 +116,14 @@ $pt=array(
 
 echo '<ul class="multicolumn nobullet">'."\n";
 $numheads=99;
+$missing=0;
 for ($i=0; $i<$numheads; $i++) {
 	$n=($i%3)*floor($numheads/3) + floor($i/3) +1;
-	echo '<li>'.$n.'. '.$pt[$n].'</li>'."\n";
+	$name=$pt[$n];
+	if (''==$name)$missing++;
+	echo '<li>'.$n.'. '.$name.'</li>'."\n";
 }
 echo "</ul>\n";
 echo '<div class="clearer"></div>'."\n";
-echo '<p>If you know - or are - any of the unidentified persons, please <a href="'.local_url('contact').'">drop us a line</a>.</p>';
+echo '<p>If you know - or are - any of the '.$missing.' unidentified persons (or notice some other irregularities), please <a href="'.local_url('contact').'">drop us a line</a>.</p>';
 #echo '<p>The picture was taken right after Dave Phillips\' <a href="'.local_url('program','pdb_filterid=27').'">keynote</a> address, not everybody that came to the conference is in this picture. Sorry, we won\'t <em><a href="http://www.gimp.org/" rel="external">gimp</a></em> you in, your next chance will be LAC\'13 at IEM, Graz.</p>';
